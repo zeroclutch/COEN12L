6c761bfd5dfc2924ce2a0643bdc05c85bbdd5e4f
P G <35671110+zeroclutch@users.noreply.github.com>
2021-03-30 17:48:38 -0700

    Fix segfault

--- Lab1/count.c
+++ Lab1/count.c
@@ -3,6 +3,11 @@
 # define MAX_WORD_LENGTH 30
 
 int main(int argc, char* argv[]) {
+    // Accept file as first command line argument
+    if(argc < 2) {
+        printf("Error: File not found.\n");
+        return 1;
+    }
 
     char* COUNTING_FILE = argv[1];
     FILE* file = fopen(COUNTING_FILE, "r");
@@ -17,7 +22,7 @@ int main(int argc, char* argv[]) {
             fscanf(file, "%s", &name);
             count++;
         }
-        printf("%d total words", count);
+        printf("%d total words\n", count);
+        fclose(file);
     }
-    fclose(file);
 }
